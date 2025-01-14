e77365b57c531046cdbc44e4a697f59fbb507f4b
dev-yihyun <dev_yihyun@naver.com>
2025-01-14 17:33:34 +0900

    [refactor] Join Page 비즈니스 로직 분리
    [description]
    - Join 페이지 안에 있는 로직을 useJoin 훅 사용

--- src/page/Join/main.js
+++ src/page/Join/main.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import Button from "../../component/Button/Button";
 import IconButton from "../../component/Button/IconButton";
@@ -9,97 +8,28 @@ import FlexBox from "../../component/Layout/FlexBox";
 import Page from "../../component/Layout/Page";
 import Text from "../../component/Text/Text";
 import Title from "../../component/Text/Title";
-import { formatPhoneNumber } from "../../function/formatPhoneNumber";
+import { useJoin } from "./hook/useJoin";
 function Join() {
-    const [inputID, setInputID] = useState("");
-    const [inputPW, setInputPW] = useState("");
-    const [inputCheckPW, setInputCheckPW] = useState("");
-    const [inputIDErrorMessage, setInputIDErrorMessage] = useState("");
-    const [inputPWErrorMessage, setInputPWErrorMessage] = useState("");
-    const [inputName, setInputName] = useState("");
-    const [inputPhone, setInputPhone] = useState("");
-    const [checkPhone, setCheckPhone] = useState(false);
-    const [inputEmail, setInputEmail] = useState("");
-    const [inputEmailErrorMessage, setInputEmailErrorMessage] = useState("");
-
-    const regexID = /^[a-zA-Z0-9]*$/;
-    const regexPW = /^[a-zA-Z0-9!@#$%^&*+\-=_?]*$/;
-    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    const onInputID = (event) => {
-        setInputID(event.target.value);
-        if (!regexID.test(event.target.value)) {
-            setInputIDErrorMessage("아이디는 영어 대소문자와 숫자만 가능합니다.");
-        } else {
-            setInputIDErrorMessage("");
-        }
-    };
-
-    const onIDCheck = () => {
-        alert("클릭");
-    };
-
-    const onInputPW = (event) => {
-        setInputPW(event.target.value);
-        if (!regexPW.test(event.target.value)) {
-            setInputPWErrorMessage(
-                "비밀번호는 영어,숫자,특수문자(!@#$%^&*+-=_?)만 입력 가능합니다."
-            );
-        } else {
-            setInputPWErrorMessage("");
-        }
-    };
-
-    const onInputCheckPW = (event) => {
-        setInputCheckPW(event.target.value);
-
-        if (
-            event.target.value !== inputPW ||
-            !regexPW.test(event.target.value) ||
-            !regexPW.test(inputPW)
-        ) {
-            setInputPWErrorMessage("비밀번호가 일치하지 않습니다.");
-        } else {
-            setInputPWErrorMessage("");
-        }
-    };
-
-    const onInputName = (event) => {
-        setInputName(event.target.value);
-    };
-
-    const onInputPhone = (event) => {
-        const formattedValue = formatPhoneNumber(event.target.value);
-        setInputPhone(formattedValue);
-        setCheckPhone(!/^\d{3}-\d{4}-\d{4}$/.test(formattedValue));
-        if (event.target.value === "") {
-            setCheckPhone(false);
-        }
-    };
-
-    const onInputEmail = (event) => {
-        setInputEmail(event.target.value);
-        if (!regexEmail.test(event.target.value)) {
-            setInputEmailErrorMessage("이메일 주소를 정확히 입력해주세요.");
-        } else {
-            setInputEmailErrorMessage("");
-        }
-    };
-
-    const isFormValid = () => {
-        return (
-            inputID.trim() &&
-            !inputIDErrorMessage &&
-            inputPW.trim() &&
-            !inputPWErrorMessage &&
-            inputName.trim() &&
-            inputPhone.trim() &&
-            !checkPhone &&
-            inputEmail.trim() &&
-            !inputEmailErrorMessage &&
-            regexEmail.test(inputEmail)
-        );
-    };
+    const {
+        inputId,
+        inputPw,
+        inputCheckPw,
+        inputIdErrorMessage,
+        inputPwErrorMessage,
+        inputName,
+        inputPhone,
+        checkPhone,
+        inputEmail,
+        inputEmailErrorMessage,
+        onInputId,
+        onIdCheck,
+        onInputPw,
+        onInputCheckPw,
+        onInputName,
+        onInputPhone,
+        onInputEmail,
+        isFormValid,
+    } = useJoin();
 
     return (
         <>
@@ -113,38 +43,38 @@ function Join() {
                         <FlexBox gap="8px">
                             <Input
                                 placeholder="ID"
-                                value={inputID}
-                                onChange={onInputID}
+                                value={inputId}
+                                onChange={onInputId}
                                 maxLength="16"
-                                error={inputIDErrorMessage}
+                                error={inputIdErrorMessage}
                             />
-                            {inputIDErrorMessage && (
+                            {inputIdErrorMessage && (
                                 <Text color="error" alignSelf="flex-start">
-                                    {inputIDErrorMessage}
+                                    {inputIdErrorMessage}
                                 </Text>
                             )}
                             <Button
-                                disabled={!inputID || !!inputIDErrorMessage}
-                                onClick={onIDCheck}
+                                disabled={!inputId || !!inputIdErrorMessage}
+                                onClick={onIdCheck}
                             >
                                 Check ID
                             </Button>
                             <Password
                                 placeholder="Password"
-                                value={inputPW}
-                                onChange={onInputPW}
+                                value={inputPw}
+                                onChange={onInputPw}
                                 maxLength="16"
                             />
                             <Password
                                 placeholder="Check Password"
-                                value={inputCheckPW}
-                                onChange={onInputCheckPW}
+                                value={inputCheckPw}
+                                onChange={onInputCheckPw}
                                 maxLength="16"
                             />
 
-                            {inputPWErrorMessage && (
+                            {inputPwErrorMessage && (
                                 <Text alignSelf="flex-start" color="error">
-                                    {inputPWErrorMessage}
+                                    {inputPwErrorMessage}
                                 </Text>
                             )}
                             <Input
