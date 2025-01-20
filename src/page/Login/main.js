ccd15ad9bdadf03da02a2130061b259e5fe44d37
dev-yihyun <dev_yihyun@naver.com>
2025-01-20 14:10:43 +0900

    [fix] Login Page 오류 수정

--- src/page/Login/main.js
+++ src/page/Login/main.js
@@ -4,6 +4,7 @@ import Input from "../../component/Input/Input";
 import FlexBox from "../../component/Layout/FlexBox";
 import Page from "../../component/Layout/Page";
 import Space from "../../component/Layout/Space";
+import TextLink from "../../component/Text/TextLink";
 import Title from "../../component/Text/Title";
 
 function Login() {
@@ -16,6 +17,37 @@ function Login() {
                         <Input placeholder="ID" />
                         <Input placeholder="PW" />
                         <Button>Login</Button>
+
+                        <Space height="1" />
+                        <TextLink
+                            text="Did you forget your account? {link} or {link}"
+                            links={[
+                                {
+                                    to: "/FindId",
+                                    label: "ID",
+                                    color: "#0D47A1",
+                                    fontWeight: "bold",
+                                },
+                                {
+                                    to: "/FindPW",
+                                    label: "Password",
+                                    color: "#0D47A1",
+                                    fontWeight: "bold",
+                                },
+                            ]}
+                        />
+                        <Space height="1" />
+                        <TextLink
+                            text="You're not a member yet? {link}"
+                            links={[
+                                {
+                                    to: "/Join",
+                                    label: "Sign up for membership>",
+                                    fontWeight: "bold",
+                                    color: "#212121",
+                                },
+                            ]}
+                        />
                     </FlexBox>
                     <Space height="10" />
                 </Card>
