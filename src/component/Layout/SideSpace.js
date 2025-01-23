291cbd65f0a974454bf535bb12a8ed2885a0f43d
dev-yihyun <dev_yihyun@naver.com>
2025-01-23 22:02:33 +0900

    [fix] SideSpace 컴포넌트 수정

--- src/component/Layout/SideSpace.js
+++ src/component/Layout/SideSpace.js
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const SideSpaceComponent = styled.div`
-    margin-left: ${({ margin }) => (margin ? margin : "0px")};
-    margin-right: ${({ margin }) => (margin ? margin : "0px")};
+    margin-left: ${({ $margin }) => ($margin ? $margin : "0px")};
+    margin-right: ${({ $margin }) => ($margin ? $margin : "0px")};
 `;
 
 function SideSpace({ children, margin = "0px" }) {
-    return <SideSpaceComponent margin={margin}>{children}</SideSpaceComponent>;
+    return <SideSpaceComponent $margin={margin}>{children}</SideSpaceComponent>;
 }
 export default React.memo(SideSpace);
