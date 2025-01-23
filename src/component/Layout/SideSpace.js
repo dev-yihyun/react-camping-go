8a59a59d5d3215d5a07ce8865bc79d4bc108e141
dev-yihyun <dev_yihyun@naver.com>
2025-01-23 16:52:16 +0900

    [feat] SideSpace 컴포넌트 수정

--- src/component/Layout/SideSpace.js
+++ src/component/Layout/SideSpace.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const SideSpaceComponent = styled.div`
-margin-left:${({ margin }) => (margin ? margin : "0px")}
-margin-right:${({ margin }) => (margin ? margin : "0px")}
+    margin-left: ${({ margin }) => (margin ? margin : "0px")};
+    margin-right: ${({ margin }) => (margin ? margin : "0px")};
 `;
 
 function SideSpace({ children, margin = "0px" }) {
