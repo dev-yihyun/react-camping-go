1f904d1b1b7f00bad6848af83c2e7483732636cb
dev-yihyun <dev_yihyun@naver.com>
2025-01-24 16:20:01 +0900

    [feat] Footer 컴포넌트

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const FooterComponent = styled.footer`
+    // background-color: #4caf50;
+    background-color: #004d40;
+    color: white;
+    padding: 10px 0;
+    text-align: center;
+`;
+
+function Footer() {
+    return (
+        <>
+            <FooterComponent>
+                <p>&copy; 2025 Camping Go. All rights reserved.</p>
+            </FooterComponent>
+        </>
+    );
+}
+
+export default Footer;
