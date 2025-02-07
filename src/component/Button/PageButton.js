56d9a9ef3117c62b9ba1d00eb5038c0a2b3206c1
dev-yihyun <dev_yihyun@naver.com>
2025-02-07 19:54:48 +0900

    [fix] PageButton 컴포넌트 수정

--- src/component/Button/PageButton.js
+++ src/component/Button/PageButton.js
@@ -1,22 +1,36 @@
+import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
 
 const ButtonComponent = styled.button`
     border: 3px solid #9ab3a0;
     border-radius: 50px;
-    background-color: #ffffff;
     font-size: 15px;
     cursor: pointer;
     font-weight: bold;
     padding: 10px;
+    background-color: ${({ active }) => (active ? "#D9E4CC " : "#ffffff")};
 `;
 
-function PageButton({ children }) {
+function PageButton({ children, onClick, active }) {
+    console.log("##");
     return (
         <>
-            <ButtonComponent>{children}</ButtonComponent>
+            <ButtonComponent onClick={onClick} active={active}>
+                {children}
+            </ButtonComponent>
         </>
     );
 }
+
+PageButton.propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+};
+
+PageButton.defaultProps = {
+    children: "Button",
+    onClick: () => {},
+};
 
 export default React.memo(PageButton);
